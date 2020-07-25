a5fd6817e38c4c3a9af8e984a30397cb2ac2391f
Andre Dufresne <a_dufresne@coloradocollege.edu>
2020-07-25 14:39:11 -0600

    Added comments to code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1; // set slide index
+showSlides(slideIndex); //this will open the page with the first slide
 
+// Build response to pressing of buttons
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
@@ -12,10 +13,14 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName('slide');
-    if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i=0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-    slides[slideIndex-1].style.display = 'flex';
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = 'flex';
 }
